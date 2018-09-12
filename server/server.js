c82029943f5e1ed7519208d08b8ed29409d08f87
mupamedi <mupamedi@medi.de>
2018-09-12 15:36:55 +0200

    add test cases for GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -35,7 +35,8 @@ app.get('/todos/:id', (req, res) => {
 
     if(!ObjectID.isValid(id)) {
         return res.status(404).send();
-    }; 
+    };
+     
     Todo.findById(id).then((todo) => {
         if(!todo){
             return res.status(404).send();
