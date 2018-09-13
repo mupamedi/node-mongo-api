3ca4bda4c97bc63502bec46f2d551748eed4d8c5
mupamedi <mupamedi@medi.de>
2018-09-13 09:31:46 +0200

    Add DELETE /todos/:id route

---
+++ playground/mongoose-remove.js
@@ -0,0 +1,13 @@
+const {ObjectID} = require('mongodb');
+
+const {mongoose} = require('../server/db/mongoose');
+const {Todo} = require('../server/models/todo');
+const {User} = require('../server/models/user');
+
+var id = '5b98c827b3a6833503afe65b';
+
+
+
+
+
+
--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ app.post('/todos', (req, res) => {
   });
 
   todo.save().then((doc) => {
-    res.send(doc);
+    res.send(doc); 
   }, (e) => {
     res.status(400).send(e);
   });
@@ -43,6 +43,22 @@ app.get('/todos/:id', (req, res) => {
             return res.status(404).send();
         }
         res.send({todo});        
+    }).catch((e) => {
+        return res.status(400).send();
+    });
+});
+
+app.delete('/todos/:id', (req, res) => {
+    var id = req.params.id;
+
+    if(!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    };
+    Todo.findByIdAndRemove(id).then((todo) =>{
+        if(!todo){
+            return res.status(404).send();
+        }
+        res.send({todo});
     }).catch((e) => {
         return res.status(400).send();
     });
