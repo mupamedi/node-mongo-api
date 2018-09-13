1de3485034a92351d6694a062f1f7620c56a2926
mupamedi <mupamedi@medi.de>
2018-09-13 11:57:21 +0200

    security fix

--- server/server.js
+++ server/server.js
@@ -1,3 +1,17 @@
+var env = process.env.NODE_ENV || 'development';
+console.log('env ******', env);
+
+
+if(env === 'development') {
+    process.env.PORT = 3000;
+    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodosApp';
+} else if (env === 'test') {
+    process.env.PORT = 3000;
+    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodosAppTest';
+}else if(env === 'production') {
+    process.env.MONGODB_URI = 'mongodb://kweoizvbkawg:hzDMVWXXzwf4p7dZMXf4TMiZzkwkdHmsfzefnRCJvrWGVefVtAMqp9y6fvWroQNU@ds155352.mlab.com:55352/node-api-test'
+}
+
 const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -8,7 +22,7 @@ var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
 var app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 app.use(bodyParser.json());
 
