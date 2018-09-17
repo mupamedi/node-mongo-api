f744a8efa1e9fb32e4b99ad2128afbc3a8fc134c
mupamedi <githubmedi@muck.email>
2018-09-17 15:31:20 +0200

    Update config to use json file

--- server/config/config.js
+++ server/config/config.js
@@ -1,12 +1,10 @@
 var env = process.env.NODE_ENV || 'development';
 
-
-if(env === 'development') {
-    process.env.PORT = 3000;
-    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodosApp';
-} else if (env === 'test') {
-    process.env.PORT = 3000;
-    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodosAppTest';
-}else if(env === 'production') {
-    process.env.MONGODB_URI = 'mongodb://kweoizvbkawg:hzDMVWXXzwf4p7dZMXf4TMiZzkwkdHmsfzefnRCJvrWGVefVtAMqp9y6fvWroQNU@ds155352.mlab.com:55352/node-api-test'
+if(env === 'development' || env === 'test') {
+    var config = require('./config.json');
+    var envConfig = config[env];
+    
+    Object.keys(envConfig).forEach((key) => {
+        process.env[key] = envConfig[key];
+    })
 }
